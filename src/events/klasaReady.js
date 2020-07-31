687095f2639fd8e34ecb06208f338a7a93fd7458
Dwigoric <dwigoric@gmail.com>
2020-07-31 21:07:01 +0800

    misc(status): remove name

--- src/events/klasaReady.js
+++ src/events/klasaReady.js
@@ -3,7 +3,7 @@ const { Event } = require('klasa');
 module.exports = class extends Event {
 
 	async run() {
-		this.client.user.setPresence({ status: 'online', activity: { name: 'Gwen\'s AFK Game', type: 'PLAYING' } });
+		this.client.user.setPresence({ status: 'online' });
 	}
 
 };
